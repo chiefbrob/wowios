45067806f75fdeae7f0f4c74a7cd1c61b2dc4401
Jack Greenly <jackgreenly@Chiefbrobs-iMac.local>
2019-01-19 13:05:45 +0300

    First Commit

--- WOW ME/ViewController.swift
+++ WOW ME/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet weak var webview: WKWebView!
+    
+    override func loadView(){
+        webview = WKWebView()
+        webview.navigationDelegate = self
+        view = webview
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://salon.dininghall.xyz")
+        webview.load(URLRequest(url: url!))
     }
 
 
